d5b58a075607d4dbba8a34a2231d676528098e85
xubeiyan <yueyongdong@gmail.com>
2025-03-18 15:46:25 +0800

    feature(MainFrame.svelte): add fallback image when miss game icon

--- src/pages/MainFrame.svelte
+++ src/pages/MainFrame.svelte
@@ -9,6 +9,7 @@
 
   import { invoke } from "@tauri-apps/api/core";
   import { onMount } from "svelte";
+  import Refresh from "$svgIcon/refresh.svelte";
 
   let saveFromGameFolders = $state([]);
   let saveFromAsyncFolders = $state([]);
@@ -43,7 +44,12 @@
     $basicStore.async_folder == null ? "未能获取到" : $basicStore.async_folder
   );
 
-  onMount(() => {
+  // 没有游戏图标的时候提供替代图标
+  const handleError = (ev) => {
+    ev.target.src = "/gameIcons/default.png";
+  };
+
+  const load_conf_file = () => {
     if ($basicStore.async_folder == null) return;
 
     // 读取同步文件夹中的目录与配置
@@ -51,8 +57,13 @@
       (json_string) => {
         let content = JSON.parse(json_string);
         saveFromAsyncFolders = content.conf_file_content.game_save;
+        console.log('loaded!')
       }
     );
+  };
+
+  onMount(() => {
+    load_conf_file();
   });
 </script>
 
@@ -67,20 +78,30 @@
   <div class="mt-4 grow text-4th grid grid-cols-[1fr_160px_1fr]">
     <div class="flex flex-col gap-[12px]">
       <p class="text-xl">同步文件夹存档</p>
-      <div class="bg-2nd/50 rounded-[10px] grow mb-[20px] p-[12px]">
+      <div
+        class="bg-2nd/50 rounded-[10px] overflow-y-auto max-h-[300px] grow mb-[20px] py-[4px] pl-[12px] pr-[7px] flex flex-col gap-[4px]"
+      >
+        <div class="flex justify-end">
+          <button onclick={load_conf_file}>
+            <Refresh />
+          </button>
+        </div>
         {#each saveFromAsyncFolders as one, id}
           <button
-            class="flex items-center gap-[16px]
+            class="w-full flex items-center gap-[16px]
           border-2 border-3rd rounded-[10px] p-[8px] {leftSelectStyle(id)}"
             onclick={() => selectGame({ side: "left", id })}
           >
             <img
               class="size-[75px] rounded-[7px]"
-              src="/gameIcons/1.jpg"
+              src="/gameIcons/{one.db_id}.jpg"
               alt={one.name}
+              onerror={handleError}
             />
             <div class="flex flex-col items-start text-sm">
-              <span class="font-bold text-left">{one.name}</span>
+              <div class="h-[2.5rem] text-left">
+                <span class="font-bold">{one.name}</span>
+              </div>
               <span class="text-2nd-text">最后更新时间</span>
               <span class="text-2nd-text">{one.last_update_time}</span>
             </div>
