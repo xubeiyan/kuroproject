f04009306a8126be97797459f61e35f81a3169ee
xubeiyan <yueyongdong@gmail.com>
2025-03-19 09:06:14 +0800

    feature(TitleBar.svelte): add minimize button

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -2,11 +2,19 @@
   import { getCurrentWindow } from "@tauri-apps/api/window";
   const appWindow = getCurrentWindow();
   import CloseIcon from "../assets/svg-icon/close.svelte";
+  import MinimizeIcon from "$svgIcon/minimize.svelte";
 </script>
 
 <div class="bg-3rd h-[40px] px-[15px] flex justify-end" data-tauri-drag-region>
   <button
-    class="bg-alert hover:bg-alert-light w-[60px] h-[30px] flex justify-center items-center rounded-[5px]"
+    class="bg-2nd w-[60px] h-[30px] flex justify-center items-center rounded-l-[5px]"
+    onclick={() => appWindow.minimize()}
+  >
+    <MinimizeIcon />
+  </button>
+  <button
+    class="bg-alert hover:bg-alert-light
+    w-[60px] h-[30px] flex justify-center items-center rounded-r-[5px]"
     onclick={() => appWindow.close()}
   >
     <CloseIcon />
